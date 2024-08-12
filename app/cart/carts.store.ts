9082f52a90556c7844fbbd16cce24a3f1d2db9e8
codeflamer <emryzs01@gmail.com>
2024-08-13 02:09:08 +0300

    project completed..

--- app/cart/carts.store.ts
+++ app/cart/carts.store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface ProductCart {
   cartItems: (ProductType & { count: number })[];
@@ -9,87 +10,98 @@ interface ProductCart {
   decreaseItemCount: (id: number) => void;
 }
 
-export const useProductCart = create<ProductCart>((set, get) => ({
-  cartItems: [],
-  totalAmount: 0,
-  addItemToCart: (newItem: ProductType) => {
-    const { cartItems } = get(); //get all the items in the cart
-    let currentItem: ProductType & { count: number } = { ...newItem, count: 1 }; //if we are adding item the first time, creat new key count with value 1
-    const itemexist = cartItems.find((item) => item.id == newItem.id); // try to get item if it already exists
-    if (itemexist) {
-      //if item already exists, increase the value of count
-      currentItem = { ...itemexist, count: itemexist.count + 1 };
+export const useProductCart = create<ProductCart>()(
+  persist(
+    (set, get) => ({
+      cartItems: [],
+      totalAmount: 0,
+      addItemToCart: (newItem: ProductType) => {
+        const { cartItems } = get(); //get all the items in the cart
+        let currentItem: ProductType & { count: number } = {
+          ...newItem,
+          count: 1,
+        }; //if we are adding item the first time, creat new key count with value 1
+        const itemexist = cartItems.find((item) => item.id == newItem.id); // try to get item if it already exists
+        if (itemexist) {
+          //if item already exists, increase the value of count
+          currentItem = { ...itemexist, count: itemexist.count + 1 };
+        }
+        //filter the old item from the cart and add a new one to the right
+        set((state) => ({
+          cartItems: [
+            currentItem,
+            ...state.cartItems.filter((item) => item.id !== newItem.id),
+          ],
+        }));
+        set((state) => ({
+          totalAmount: state.cartItems.reduce(
+            (acc, item) => acc + item.count * item.price,
+            0
+          ),
+        }));
+      },
+      deleteFromCart: (id: number) => {
+        const { cartItems: oldCartItems } = get();
+        const filteredItems = oldCartItems.filter((item) => item.id !== id);
+        set({ cartItems: filteredItems });
+        set((state) => ({
+          totalAmount: state.cartItems.reduce(
+            (acc, item) => acc + item.count * item.price,
+            0
+          ),
+        }));
+      },
+      increaseItemCount: (id: number) => {
+        // const { cartItems } = get();
+        // const updatedItemCount =
+
+        set((state) => ({
+          cartItems: [
+            ...state.cartItems.map((item) => {
+              if (item.id === id) {
+                item.count += 1;
+              }
+              return item;
+            }),
+          ],
+        }));
+
+        set((state) => ({
+          totalAmount: state.cartItems.reduce(
+            (acc, item) => acc + item.count * item.price,
+            0
+          ),
+        }));
+      },
+      decreaseItemCount: (id: number) => {
+        const { cartItems, deleteFromCart } = get();
+        const item = cartItems.find((item) => item.id == id);
+        if (item!.count > 1) {
+          set((state) => ({
+            cartItems: [
+              ...state.cartItems.map((item) => {
+                if (item.id === id) {
+                  item.count -= 1;
+                }
+
+                return item;
+              }),
+            ],
+          }));
+          set((state) => ({
+            totalAmount: state.cartItems.reduce(
+              (acc, item) => acc + item.count * item.price,
+              0
+            ),
+          }));
+        } else {
+          deleteFromCart(id);
+        }
+      },
+    }),
+    {
+      name: "EcommerceScooter",
+      getStorage: () => localStorage,
     }
-    //filter the old item from the cart and add a new one to the right
-    set((state) => ({
-      cartItems: [
-        currentItem,
-        ...state.cartItems.filter((item) => item.id !== newItem.id),
-      ],
-    }));
-    set((state) => ({
-      totalAmount: state.cartItems.reduce(
-        (acc, item) => acc + item.count * item.price,
-        0
-      ),
-    }));
-  },
-  deleteFromCart: (id: number) => {
-    const { cartItems: oldCartItems } = get();
-    const filteredItems = oldCartItems.filter((item) => item.id !== id);
-    set({ cartItems: filteredItems });
-    set((state) => ({
-      totalAmount: state.cartItems.reduce(
-        (acc, item) => acc + item.count * item.price,
-        0
-      ),
-    }));
-  },
-  increaseItemCount: (id: number) => {
-    // const { cartItems } = get();
-    // const updatedItemCount =
-
-    set((state) => ({
-      cartItems: [
-        ...state.cartItems.map((item) => {
-          if (item.id === id) {
-            item.count += 1;
-          }
-          return item;
-        }),
-      ],
-    }));
-
-    set((state) => ({
-      totalAmount: state.cartItems.reduce(
-        (acc, item) => acc + item.count * item.price,
-        0
-      ),
-    }));
-  },
-  decreaseItemCount: (id: number) => {
-    const { cartItems, deleteFromCart } = get();
-    const item = cartItems.find((item) => item.id == id);
-    if (item!.count > 1) {
-      set((state) => ({
-        cartItems: [
-          ...state.cartItems.map((item) => {
-            if (item.id === id) {
-              item.count -= 1;
-            }
-
-            return item;
-          }),
-        ],
-      }));
-      set((state) => ({
-        totalAmount: state.cartItems.reduce(
-          (acc, item) => acc + item.count * item.price,
-          0
-        ),
-      }));
-    } else {
-      deleteFromCart(id);
-    }
-  },
-}));
+  )
+);
